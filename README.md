096824afa106fe2d56d7a4c21e776e7ad74a1d1d
shadcn <m@shadcn.com>
2024-07-17 22:26:18 +0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Source https://twitter.com/MozzyFX/status/1813154329416568966
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { BattleFieldChart } from "@/components/battlefield-chart"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <BattleFieldChart />
+    </main>
+  )
+}
